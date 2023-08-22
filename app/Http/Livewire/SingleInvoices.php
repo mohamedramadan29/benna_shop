2ebfcc87ccc635b68f3f0c33884d169b18d65267
mohamed <mr319242@gmail.com>
2023-08-22 09:03:08 +0300

    new

---
+++ app/Http/Livewire/SingleInvoices.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class SingleInvoices extends Component
+{
+    public function render()
+    {
+        return view('livewire.single-invoices');
+    }
+}
